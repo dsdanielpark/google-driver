364f584ea359cb601c2263728f1dfd2cc6e75424
parkminwoo <parkminwoo1991@gmail.com>
2023-03-30 19:14:27 +0900

    feat: downloader off line checker

--- googledriver/downloader.py
+++ googledriver/downloader.py
@@ -1,4 +1,8 @@
 import requests
+import os
+import shutil
+# logger = logging.get_logger(__name__)  # pylint: disable=invalid-name
+
 
 def download(URL: str, local_storage_full_path: str) -> None:
     """Just put the full file path in the local area and the Google Drive file path accessible to everyone, and you can download it.
@@ -41,3 +45,38 @@ def save_file(response: str, local_storage_full_path: str):
             if chunk: 
                 f.write(chunk)
 
+
+# https://github.com/huggingface/transformers/utils/hub
+_is_offline_mode = True if os.environ.get("TRANSFORMERS_OFFLINE", "0").upper() in ENV_VARS_TRUE_VALUES else False
+
+def is_offline_mode():
+    return _is_offline_mode
+
+torch_cache_home = os.getenv("TORCH_HOME", os.path.join(os.getenv("XDG_CACHE_HOME", "~/.cache"), "torch"))
+old_default_cache_path = os.path.join(torch_cache_home, "transformers")
+# New default cache, shared with the Datasets library
+hf_cache_home = os.path.expanduser(
+    os.getenv("HF_HOME", os.path.join(os.getenv("XDG_CACHE_HOME", "~/.cache"), "huggingface"))
+)
+default_cache_path = os.path.join(hf_cache_home, "hub")
+
+# Onetime move from the old location to the new one if no ENV variable has been set.
+if (
+    os.path.isdir(old_default_cache_path)
+    and not os.path.isdir(default_cache_path)
+    and "PYTORCH_PRETRAINED_BERT_CACHE" not in os.environ
+    and "PYTORCH_TRANSFORMERS_CACHE" not in os.environ
+    and "TRANSFORMERS_CACHE" not in os.environ
+):
+    logger.warning(
+        "In Transformers v4.0.0, the default path to cache downloaded models changed from"
+        " '~/.cache/torch/transformers' to '~/.cache/huggingface/transformers'. Since you don't seem to have"
+        " overridden and '~/.cache/torch/transformers' is a directory that exists, we're moving it to"
+        " '~/.cache/huggingface/transformers' to avoid redownloading models you have already in the cache. You should"
+        " only see this message once."
+    )
+    shutil.move(old_default_cache_path, default_cache_path)
+
+PYTORCH_PRETRAINED_BERT_CACHE = os.getenv("PYTORCH_PRETRAINED_BERT_CACHE", default_cache_path)
+PYTORCH_TRANSFORMERS_CACHE = os.getenv("PYTORCH_TRANSFORMERS_CACHE", PYTORCH_PRETRAINED_BERT_CACHE)
+HUGGINGFACE_HUB_CACHE = os.getenv("HUGGINGFACE_HUB_CACHE", PYTORCH_TRANSFORMERS_CACHE)
