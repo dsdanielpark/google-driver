ca77d40ac47e7de90f084cb93d21a31bab53a1d2
parkminwoo <parkminwoo1991@gmail.com>
2023-04-03 17:24:48 +0900

    build: 0.1.4 chore

--- googledriver/__init__.py
+++ googledriver/__init__.py
@@ -2,4 +2,4 @@
 from googledriver.downloader import download, get_token, save_file, is_offline_mode, try_to_load_from_cache, cached_file, get_cachefile_from_driver
 
 __all__ = [download, get_token, save_file, is_offline_mode, try_to_load_from_cache, cached_file, get_cachefile_from_driver]
-__version__=['0.1.3']
+__version__=['0.1.4']
